24b09f110577db3bf160a42f154c5a9e7ccc8eaf
TheSwirlingVoid <rnsugavanam@gmail.com>
2022-08-18 19:26:51 -0700

    Added PipeLink class

---
+++ src/main/java/com/theswirlingvoid/polarmachinery/block/block/thermalpipe/PipeLink.java
@@ -0,0 +1,6 @@
+package com.theswirlingvoid.polarmachinery.block.block.thermalpipe;
+
+public class PipeLink {
+	// TODO: Implement this class
+	// it's a bridge between connection ends and pipes for transferring heat
+}
